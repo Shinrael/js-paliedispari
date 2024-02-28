c661b815a920f6b4b117402fddd326733a64491f
Shinrael <mircoelena89@gmail.com>
2024-02-28 17:31:06 +0100

    [add] edit

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@
 // Creo la funzione 
 
 function palindroma(word) {
+
+  // Utilizzo for per confrontare i caratteri
+  
   for(i = 0; i < word.length / 2; i++)
+
     if (word[i] !== word[word.length - 1 - i]) {
       return false;
     }
+
   return true;  
 }
 
